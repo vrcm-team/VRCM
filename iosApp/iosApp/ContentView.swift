b5c74d9240bda77952dda359caad006f8bf999be
kamosama <837080904@qq.com>
2024-03-18 12:25:07 +0800

    feat: 适配ios安全区域

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -8,14 +8,6 @@
 
 import SwiftUI
 import SwiftData
-//
-//struct ContentView: View {
-//    var body: some View {
-//        VStack{
-//            Text("Greeting().greet()")
-//        }.padding()
-//    }
-//}
 import ComposeApp
 
 struct ComposeView: UIViewControllerRepresentable {
