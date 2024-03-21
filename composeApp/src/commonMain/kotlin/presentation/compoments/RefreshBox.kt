bfa8e0e46d245107489a7e64c5aacf2b5c8ae446
kamosama <837080904@qq.com>
2024-03-21 18:06:28 +0800

    refactor: 抽离FriendLocation页面成独立Tab逻辑

---
+++ composeApp/src/commonMain/kotlin/presentation/compoments/RefreshBox.kt
@@ -0,0 +1,53 @@
+package io.github.vrcmteam.vrcm.presentation.compoments
+
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
+import androidx.compose.material3.pulltorefresh.PullToRefreshState
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+
+/**
+ * 下拉刷新Box组件
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RefreshBox(
+    modifier: Modifier = Modifier,
+    isStartRefresh : Boolean = true,
+    pullToRefreshState: PullToRefreshState = rememberPullToRefreshState(),
+    doRefresh: suspend () -> Unit = {},
+    content: @Composable BoxScope.() -> Unit
+) {
+    if (isStartRefresh) {
+        LaunchedEffect(Unit){
+            pullToRefreshState.startRefresh()
+        }
+    }
+    val scaleFraction = if (pullToRefreshState.isRefreshing) 1f else
+        LinearOutSlowInEasing.transform(pullToRefreshState.progress).coerceIn(0f, 1f)
+    if (pullToRefreshState.isRefreshing) {
+        LaunchedEffect(Unit) {
+            doRefresh()
+            pullToRefreshState.endRefresh()
+        }
+    }
+    Box(
+        modifier.nestedScroll(pullToRefreshState.nestedScrollConnection)
+    ) {
+        content()
+        PullToRefreshContainer(
+            modifier = Modifier
+                .graphicsLayer(scaleX = scaleFraction, scaleY = scaleFraction)
+                .align(Alignment.TopCenter),
+            state = pullToRefreshState,
+        )
+    }
+}
