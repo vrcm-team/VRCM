eb1c50e59eaa44620bb1540885cc8930c4ff7ed9
kamosama <837080904@qq.com>
2024-04-28 11:52:38 +0800

    fix: 给NotificationApi加runCatching防止超时直接闪退app

--- composeApp/src/commonMain/kotlin/network/api/notification/NotificationApi.kt
+++ composeApp/src/commonMain/kotlin/network/api/notification/NotificationApi.kt
@@ -3,28 +3,25 @@ package io.github.vrcmteam.vrcm.network.api.notification
 import io.github.vrcmteam.vrcm.network.api.attributes.NOTIFICATIONS_API_PREFIX
 import io.github.vrcmteam.vrcm.network.api.notification.data.NotificationData
 import io.github.vrcmteam.vrcm.network.extensions.ifOK
-import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.request.get
-import io.ktor.client.request.parameter
-import io.ktor.client.request.post
-import io.ktor.client.request.setBody
-import io.ktor.client.statement.bodyAsText
-import io.ktor.http.ContentType
-import io.ktor.http.content.TextContent
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.http.content.*
 import network.api.notification.data.ResponseData
 
 class NotificationApi(
     private val client: HttpClient,
 ) {
 
-    suspend fun fetchNotifications(limit: Int = 100 ): Result<List<NotificationData>> {
-        return client.get(NOTIFICATIONS_API_PREFIX) {
+    suspend fun fetchNotifications(limit: Int = 100 ): Result<List<NotificationData>> = runCatching {
+        client.get(NOTIFICATIONS_API_PREFIX) {
             parameter("limit", limit)
-        }.ifOK { body() }
+        }.ifOK<List<NotificationData>> { body() }.getOrThrow()
     }
 
-    suspend fun responseNotification(id: String, response: ResponseData): Result<String> {
+    suspend fun responseNotification(id: String, response: ResponseData): Result<String> = runCatching {
         return client.post("$NOTIFICATIONS_API_PREFIX/$id/respond"){
             setBody(TextContent("""{"responseData":"${response.responseData}","responseType":"${response.type}"}""", ContentType.Application.Json))
         }.ifOK { bodyAsText() }
