67b1e03a2eb20a373cbb9a3b81980b1b063c5153
kamosama <837080904@qq.com>
2024-05-05 18:58:34 +0800

    update: README.md

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -26,7 +26,7 @@ kotlin {
         iosSimulatorArm64()
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
-            baseName = "VRCM"
+            baseName = "ComposeApp"
             isStatic = true
         }
     }
