63e9f573561acb43e77c88967fc5bed366a45214
卡莫sama <837080904@qq.com>
2024-05-08 01:39:30 +0800

    feat:增加LocationCard好友缩略图展开时的动画效果

--- composeApp/src/commonMain/kotlin/presentation/screens/home/pager/FriendLocationPager.kt
+++ composeApp/src/commonMain/kotlin/presentation/screens/home/pager/FriendLocationPager.kt
@@ -6,13 +6,36 @@ import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyItemScope
+import androidx.compose.foundation.lazy.LazyListScope
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Explore
-import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
@@ -347,16 +370,20 @@ private fun LazyItemScope.LocationCard(
                             .fillMaxWidth()
                             .height(24.dp),
                     ) {
-                        friendList.take(5).forEachIndexed { index, state ->
-                            val friend by state
-                            UserStateIcon(
-                                modifier = Modifier
-                                    .fillMaxHeight()
-                                    .align(Alignment.CenterVertically)
-                                    .offset(x = (-8 * index).dp)
-                                    .border(1.dp, MaterialTheme.colorScheme.surface, CircleShape),
-                                iconUrl = friend.iconUrl,
-                            )
+                        AnimatedVisibility(!showUser){
+                            Row(
+                                horizontalArrangement = Arrangement.spacedBy((-8).dp)
+                            ) {
+                                friendList.take(5).forEach { friendState ->
+                                    UserStateIcon(
+                                        modifier = Modifier
+                                            .fillMaxHeight()
+                                            .align(Alignment.CenterVertically)
+                                            .border(1.dp, MaterialTheme.colorScheme.surface, CircleShape),
+                                        iconUrl = friendState.value.iconUrl,
+                                    )
+                                }
+                            }
                         }
                         Spacer(modifier = Modifier.weight(1f))
                         Box(
