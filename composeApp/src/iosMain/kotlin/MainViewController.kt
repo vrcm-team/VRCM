51d00779550d6dfdb986aa60bbeb951419fc2d99
kamosama <837080904@qq.com>
2024-05-14 17:55:49 +0800

    update:更新IOS的启动顺序

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -4,11 +4,12 @@ import androidx.compose.ui.window.ComposeUIViewController
 import io.github.vrcmteam.vrcm.di.commonModules
 import io.github.vrcmteam.vrcm.di.modules.platformModule
 import org.koin.core.context.startKoin
-
-fun MainViewController() = run {
-    startKoin {
-        printLogger()
-        modules(commonModules + platformModule)
-    }
+@Suppress("unused")
+fun MainViewController() =run  {
     ComposeUIViewController { App() }
+}
+
+fun StartKoin() = startKoin {
+    printLogger()
+    modules(commonModules + platformModule)
 }
--- iosApp/iosApp/iosAppApp.swift
+++ iosApp/iosApp/iosAppApp.swift
@@ -1,7 +1,10 @@
 import SwiftUI
-
+import ComposeApp
 @main
 struct iOSApp: App {
+    init(){
+        MainViewControllerKt.StartKoin()
+    }
 	var body: some Scene {
 		WindowGroup {
 			ContentView()
