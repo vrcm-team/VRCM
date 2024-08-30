2ed22ad0df3581b6bd33d1a209056ca6867b8a82
KAMOsama <kamosama.dev@gmail.com>
2024-08-30 17:00:16 +0800

    feat: 新增搜索用户Api

--- composeApp/src/commonMain/kotlin/network/api/users/UsersApi.kt
+++ composeApp/src/commonMain/kotlin/network/api/users/UsersApi.kt
@@ -2,16 +2,31 @@ package io.github.vrcmteam.vrcm.network.api.users
 
 import io.github.vrcmteam.vrcm.network.api.attributes.USERS_API_PREFIX
 import io.github.vrcmteam.vrcm.network.api.users.data.UserData
-import io.github.vrcmteam.vrcm.network.extensions.ifOK
-import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.request.get
+import io.github.vrcmteam.vrcm.network.extensions.ifOKOrThrow
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.request.*
+import network.api.users.data.SearchUserData
 
 class UsersApi(private val client: HttpClient) {
 
     suspend fun fetchUser(userId: String) =
-        client.get("$USERS_API_PREFIX/$userId").ifOK { body<UserData>() }
+        client.get("$USERS_API_PREFIX/$userId").ifOKOrThrow { body<UserData>() }
+
     suspend fun fetchUserResponse(userId: String) =
-        client.get("$USERS_API_PREFIX/$userId").ifOK { this }
+        client.get("$USERS_API_PREFIX/$userId").ifOKOrThrow { this }
+
+    suspend fun searchUser(
+        search: String,
+        sort: String = "relevance",
+        n: Int = 50,
+        offset: Int = 0
+    ) =
+        client.get(USERS_API_PREFIX) {
+            parameter("sort", sort)
+            parameter("n", n)
+            parameter("offset", offset)
+            parameter("search", search)
+        }.ifOKOrThrow { body<SearchUserData>() }
 
 }
