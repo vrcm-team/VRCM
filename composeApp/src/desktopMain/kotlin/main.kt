2331d65371c27746542284e0eb00107e8c4691f2
卡莫sama <837080904@qq.com>
2024-09-03 01:29:35 +0800

    fix: 修改桌面的窗口比例与初始位置

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -2,24 +2,36 @@ package io.github.vrcmteam.vrcm
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.application
+import androidx.compose.ui.window.rememberWindowState
 import io.github.vrcmteam.vrcm.di.commonModules
 import io.github.vrcmteam.vrcm.di.modules.platformModule
-import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import org.koin.core.context.startKoin
 import vrcm.composeapp.generated.resources.Res
 import vrcm.composeapp.generated.resources.logo
 
-@OptIn(ExperimentalResourceApi::class)
 fun main() = run {
     startKoin {
         printLogger()
         modules(commonModules + platformModule)
     }
     application {
-        Window(onCloseRequest = ::exitApplication, title = "VRCM", icon =  painterResource(Res.drawable.logo)) {
+        val windowState = rememberWindowState(
+            width = 400.dp,
+            height = 800.dp,
+            position = WindowPosition.Aligned(Alignment.Center)
+        )
+        Window(
+            state = windowState,
+            onCloseRequest = ::exitApplication,
+            title = "VRCM",
+            icon =  painterResource(Res.drawable.logo)
+        ) {
             App()
         }
     }
