a25ecc8de0251e0ea039787cefa4ea827c65359c
KAMOsama <kamosama.dev@gmail.com>
2024-09-18 12:15:51 +0800

    优化:在MainViewController.kt中应用了@file:Suppress注解以消除未使用的警告，同时修正了函数名称的命名。

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,10 +1,11 @@
+@file:Suppress("unused", "FunctionName")
 package io.github.vrcmteam.vrcm
 
 import androidx.compose.ui.window.ComposeUIViewController
 import io.github.vrcmteam.vrcm.di.commonModules
 import io.github.vrcmteam.vrcm.di.modules.platformModule
 import org.koin.core.context.startKoin
-@Suppress("unused")
+
 fun MainViewController() =run  {
     ComposeUIViewController { App() }
 }
