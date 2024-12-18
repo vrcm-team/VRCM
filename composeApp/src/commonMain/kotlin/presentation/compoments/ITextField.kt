c801d024f43dd5f880a0b79e44a9741ca9900037
KAMOsama <kamosama.dev@gmail.com>
2024-12-18 14:43:13 +0800

    解决无法输入空格问题
    - 修改 onValueChange 回调，移除自动 trim 功能

--- composeApp/src/commonMain/kotlin/presentation/compoments/ITextField.kt
+++ composeApp/src/commonMain/kotlin/presentation/compoments/ITextField.kt
@@ -18,11 +18,7 @@ import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -86,7 +82,7 @@ fun ITextField(
                     innerTextField()
                 }
             },
-            onValueChange = { onValueChange(it.trim()) },
+            onValueChange = { onValueChange(it) },
             singleLine = true,
             visualTransformation =  VisualTransformation.None,
             keyboardOptions = keyboardOptions,
