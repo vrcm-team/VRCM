085959ad2253634a3c62b567b3434d044310f695
KAMOsama <kamosama.dev@gmail.com>
2024-12-26 13:23:54 +0800

    优化 LocalDateTime.ignoredFormat 扩展属性的代码格式
    - 调整代码缩进和格式，提高可读性
    - 保持功能不变，仅优化代码结构

--- composeApp/src/commonMain/kotlin/presentation/extensions/LocalDateTimes.kt
+++ composeApp/src/commonMain/kotlin/presentation/extensions/LocalDateTimes.kt
@@ -4,14 +4,15 @@ import kotlinx.datetime.*
 import kotlinx.datetime.format.char
 
 val LocalDateTime.ignoredFormat: String
-    get() = format(LocalDateTime.Format{
-        val nowTime = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
-        if (nowTime.year != this@ignoredFormat.year){
-            year(); char('/')
-        }
-        if (nowTime.month != this@ignoredFormat.month || nowTime.dayOfMonth != this@ignoredFormat.dayOfMonth){
-            monthNumber(); char('/'); dayOfMonth(); char(' ')
-        }
-        hour(); char(':'); minute()
-    })
+    get() = format(
+        LocalDateTime.Format {
+            val nowTime = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
+            if (nowTime.year != this@ignoredFormat.year) {
+                year(); char('/')
+            }
+            if (nowTime.month != this@ignoredFormat.month || nowTime.dayOfMonth != this@ignoredFormat.dayOfMonth) {
+                monthNumber(); char('/'); dayOfMonth(); char(' ')
+            }
+            hour(); char(':'); minute()
+        })
 
