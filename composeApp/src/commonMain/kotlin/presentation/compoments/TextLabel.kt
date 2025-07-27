10eeedbd81062763fd63ee881c5861724d8c0690
KAMOsama <kamosama.dev@gmail.com>
2025-07-27 13:46:06 +0800

    fix: 修復主页房间人数显示溢出问题 - 将 Text 组件替换为 BasicText，以更好地处理长文本 - 添加自动调整文本大小功能，确保文本能在有限空间内显示 - 设置最大行数为1，保证文本标签的简洁性

--- composeApp/src/commonMain/kotlin/presentation/compoments/TextLabel.kt
+++ composeApp/src/commonMain/kotlin/presentation/compoments/TextLabel.kt
@@ -3,14 +3,16 @@ package io.github.vrcmteam.vrcm.presentation.compoments
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.text.TextAutoSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun TextLabel(
@@ -29,10 +31,11 @@ fun TextLabel(
             .padding(horizontal = 8.dp),
         contentAlignment = Alignment.Center
     ) {
-        Text(
+        BasicText(
             text = text,
-            style = MaterialTheme.typography.labelSmall,
-            color = color
+            style = MaterialTheme.typography.labelSmall.merge(color = color),
+            autoSize = TextAutoSize.StepBased(8.sp,MaterialTheme.typography.labelSmall.fontSize),
+            maxLines = 1
         )
     }
 }
