86b59883a1a8487321528c6555936fd515e25443
墨泠moln <yourmoln@outlook.com>
2026-09-02 08:27:35 +0800

    fix(friends): 解析邀请 API 合并冲突

--- composeApp/src/commonMain/kotlin/network/api/invite/InviteApi.kt
+++ composeApp/src/commonMain/kotlin/network/api/invite/InviteApi.kt
@@ -1,10 +1,12 @@
 package io.github.vrcmteam.vrcm.network.api.invite
 
 import io.github.vrcmteam.vrcm.network.api.attributes.INVITE_API_PREFIX
+import io.github.vrcmteam.vrcm.network.api.attributes.REQUEST_INVITE_API_PREFIX
 import io.github.vrcmteam.vrcm.network.api.attributes.VRChatResponse
 import io.github.vrcmteam.vrcm.network.api.invite.data.InviteMessageData
 import io.github.vrcmteam.vrcm.network.api.invite.data.InviteMessageType
 import io.github.vrcmteam.vrcm.network.api.invite.data.InviteMyselfData
+import io.github.vrcmteam.vrcm.network.api.invite.data.RequestInviteRequest
 import io.github.vrcmteam.vrcm.network.api.invite.data.UpdateInviteMessageRequest
 import io.github.vrcmteam.vrcm.network.extensions.checkSuccess
 import io.ktor.client.*
@@ -23,6 +25,13 @@ class InviteApi(private val client: HttpClient) {
     suspend fun inviteMyselfToInstance(instanceId: String): InviteMyselfData =
         client.post("$INVITE_API_PREFIX/myself/to/$instanceId")
             .checkSuccess()
+
+    suspend fun requestInvite(userId: String, requestSlot: Int = 0) {
+        client.post("$REQUEST_INVITE_API_PREFIX/$userId") {
+            contentType(ContentType.Application.Json)
+            setBody(RequestInviteRequest(requestSlot = requestSlot))
+        }.checkSuccess { Unit }
+    }
 
     suspend fun getInviteMessages(
         userId: String,
